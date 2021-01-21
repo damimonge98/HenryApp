e3bbe6175f3d6c6c5a2039fc29927b7ead15e626
Alejo Gschwind <alejogschwind.97@gmail.com>
2021-01-21 18:14:46 -0300

    Added express server and connected to mongodb.

---
+++ api/routes/users.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+// Get All Users
+router.get("/", (req, res, next) => {
+  res.send("Hello Word!");
+});
+// Get One User
+router.get("/:id", (req, res, next) => {
+});
+// Post One User
+router.post("/", (req, res, next) => {
+});
+// Update One User
+router.patch("/:id", (req, res, next) => {
+});
+// Delete One User
+router.delete("/:id", (req, res, next) => {
+});
+
+module.exports = router;
