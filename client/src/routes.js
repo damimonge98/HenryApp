967c608547f16b4335aaf886277df706787a0d25
Sloot04 <suyaislootmans@gmail.com>
2021-01-24 13:41:57 -0300

    fix client/src/routes

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import CrudInstructor from "./components/CRUDinstructor";
+
 
 const routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         {/* To do: Acá van las rutas del cliente.*/}
-        <Route path='/instructor' component={CrudInstructor} />
+      {/*   <Route path='/example' component={component } /> */}
       </Switch>
     </BrowserRouter>
   );
