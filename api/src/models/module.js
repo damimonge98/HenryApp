9cba95908b1b3d660e68ee7ad38a25a954bd23b0
Sloot04 <suyaislootmans@gmail.com>
2021-01-26 19:45:25 -0300

    fix modelos de lecture y module

--- api/src/models/module.js
+++ api/src/models/module.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const moduleSchema = new mongoose.Schema({
-  
+const moduleSchema = new Schema({
   title: {
     type: String,
     required: true
   },
-  lectures: { 
-    type: Number, 
-    required: true
-  },
+  lectures: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Lecture'
+  }],
   description: {
     type: String,
     required: true
-  }  
+  }
 });
 
 module.exports = mongoose.model('Module', moduleSchema);
