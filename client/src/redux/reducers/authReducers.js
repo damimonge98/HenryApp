6c354782a278ea64d86a538300459afd3ee3a684
Alejo Gschwind <alejogschwind.97@gmail.com>
2021-01-26 16:25:40 -0300

    👷 CLIENT: Added auth actions and reducers.

--- client/src/redux/reducers/authReducers.js
+++ client/src/redux/reducers/authReducers.js
@@ -1,13 +1,55 @@
-import { } from '../constants/authContants';
+import {
+  REQUEST_ACTION,
+  REQUEST_FAILED_ACTION,
+  LOGIN_ACTION,
+  REGISTER_ACTION,
+  LOGOUT_ACTION
+} from '../constants/authContants';
 
 const initialState = {
-  auth: {}
+  user: null,
+  isAuth: false,
+  loading: false,
+  error: null
 };
 
 const authReducers = (state = initialState, action) => {
   switch (action.type) {
 
+    case REQUEST_ACTION:
+      return {
+        ...state,
+        loading: true
+      };
 
+    case REQUEST_FAILED_ACTION:
+      return {
+        ...state,
+        loading: false,
+        error: action.error
+      };
+
+    case LOGIN_ACTION:
+      return {
+        ...state,
+        loading: false,
+        user: action.user,
+        isAuth: true
+      };
+
+    case REGISTER_ACTION:
+      return {
+        ...state,
+        loading: false
+      };
+
+    case LOGOUT_ACTION:
+      return {
+        ...state,
+        loading: false,
+        user: null,
+        isAuth: false
+      };
 
     default:
       return state;
