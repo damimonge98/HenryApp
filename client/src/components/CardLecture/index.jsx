57fb1d7014d98d116c8feec0df5e40c4cc59ff29
Sloot04 <suyaislootmans@gmail.com>
2021-01-26 14:12:09 -0300

    link al componente cardLecture

--- client/src/components/CardLecture/index.jsx
+++ client/src/components/CardLecture/index.jsx
@@ -1,24 +1,31 @@
 import React from "react";
-/* import { Link } from "react-router-dom"; */
+import { Link } from "react-router-dom";
 import './estilos.css'
 
 const CardLecture = ({ lecture }) => {
-    const { title, imagen, description, video } = lecture;
+    let { title, description, video, modulo, imagen } = lecture;
 
     return (
-        <div className="card">
-            <div className="center">
-                <img src={imagen} alt="Henry app" />
-            </div>
-            <div>
-                <div>
-                    <h3>{title}</h3>
+        <Link to={`/${title}`}>
+            <div className="card">
+                <div className="center">
+                    <img src={imagen} alt="Henry app" />
                 </div>
                 <div>
-                    <p>{description}</p>
+                    <div>
+                        <h3>{title}</h3>
+                    </div>
+                    <div>
+                        {Array.isArray(video)
+                            ? <h5>videos: {video.length}</h5>
+                            : <h5>{video}</h5>}
+                    </div>
+                    <div>
+                        <p>{description}</p>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
