5f15905b6bf44b148aed64490b665a18dab85d2a
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-01-27 14:45:08 -0300

    avances

--- client/src/components/ModuleCard/index.js
+++ client/src/components/ModuleCard/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-//import { Header } from '../Header/index.js';
+import { Link } from "react-router-dom";
+import './styles.css';
 
-export default function ModuleCard() {
+export default function ModuleCard({moduleCard}) {
+  let { title, lectures, description } = moduleCard
+
+
   return (
     <div>
       <div className='module'>
