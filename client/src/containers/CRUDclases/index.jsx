0e266fd4a8d126d2d632f4ca938475af2158c9a2
Le127 <nereocarbonara@gmail.com>
2021-02-01 19:30:27 -0300

    Edita Lecture, title y description

--- client/src/containers/CRUDclases/index.jsx
+++ client/src/containers/CRUDclases/index.jsx
@@ -1,14 +1,22 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import './index.css'
 
 const lectureList = () => {
-    const [allLecures, setAllLectures] = useState([{
+    const [allLectures, setAllLectures] = useState([{
         title: '',
         imagen: '',
         video: [],
         modulo: '',
+        description: '',
         _id: '',
     }]);
+    const [estadoId, setEstadoId] = useState('');
+
+    const [oneLecture, setOneLecture] = useState({
+        title: "",
+        description: ""
+    });
 
     useEffect(() => {
         getLectures();
@@ -28,31 +36,79 @@ const lectureList = () => {
         }
     };
 
+    const handleSubmit = (id) => {
+        const { title, description } = oneLecture;
+        axios.patch(`http://localhost:5000/lectures/${id}`, { title, description })
+            .then(res => {
+                getLectures(),
+                    setOneLecture({
+                        title: "",
+                        description: ""
+                    })
+            })
+    }
+
+    const handleChange = (e) => {
+        setOneLecture({
+            ...oneLecture,
+            [e.target.name]: e.target.value
+        })
+    }
+
+
     return (
         <div>
-            <table >
-                <thead >
-                    <tr >
-                        <th scope="col">Modulo</th>
-                        <th scope="col">Lecture</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        allLecures.map((lecture, index) => {
-                            const { modulo, title, _id } = lecture;
-                            return (
-                                <tr key={index}>
-                                    <td >{modulo}</td>
-                                    <td>{title}</td>
-                                    <td ><button><i className="fas fa-user-edit" /></button></td>
-                                    <td><button type="submit" onClick={() => handleDelete(_id)} > <i className="fas fa-trash-alt" /></button></td>
-                                    <td><button type="submit"><i className="fas fa-plus-circle me-2" /> Agregar video</button></td>
-                                </tr>)
-                        })}
-                </tbody>
-            </table>
-        </div>
+            <div>
+                <table >
+                    <thead >
+                        <tr >
+                            <th scope="col">Modulo</th>
+                            <th scope="col">Lecture</th>
+                            <th scope="col">Descripcion</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            allLectures.map((lecture, index) => {
+                                const { modulo, title, _id, description } = lecture;
+                                return (
+                                    <tr key={index}>
+                                        <td>{modulo}</td>
+                                        <td>{title}</td>
+                                        <td>{description}</td>
+                                        <td ><button onClick={() => setEstadoId(_id)} ><a href="#openModal"> <i className="fas fa-user-edit" /></a></button></td>
+                                        <div id="openModal" class="modalDialog">
+                                            <div>	<a href="#close" title="Close" class="close">X</a>
+                                                <h2>Editar Lecture</h2>
+                                                <form onSubmit={() => handleSubmit(estadoId)}><a href="#close" title="Close" class="close"></a>
+                                                    <p>
+                                                        Titulo
+                                                        <input name="title" onChange={e => handleChange(e)} />
+                                                    </p>
+                                                    <p>
+                                                        Descripcion
+                                                        <input name="description" onChange={e => handleChange(e)}></input>
+                                                    </p>
+                                                    <button type="submit"  >GUARDAR CAMBIOS</button>
+                                                </form>
+                                            </div>
+
+
+
+
+
+                                        </div>
+                                        <td><button type="submit" onClick={() => handleDelete(_id)} > <i className="fas fa-trash-alt" /></button></td>
+                                        <td><button type="submit"><i className="fas fa-plus-circle me-2" /> Agregar video</button></td>
+                                    </tr>)
+                            })}
+                    </tbody>
+                </table>
+            </div>
+
+
+
+        </div >
     )
 };
 
