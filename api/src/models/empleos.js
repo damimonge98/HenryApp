b24fb354243292d00a6a4b882dcd544b341e2714
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-01 15:50:23 -0300

    modelos empleo y empresa

---
+++ api/src/models/empleos.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const jobSchema = new Schema({
+  
+  enterprise: [{
+      type: Schema.Types.ObjectId,
+      ref: 'Empresa'
+    }],
+  
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  remote: {
+    type: Boolean
+  },
+  location: {
+    type: String,
+    required: true
+  },
+
+  linkedIn: {
+     type: String
+   }
+
+});
+
+module.exports = mongoose.model('Empleo', jobSchema);
