7314d15a758528b2b86fe35c85516fdd68f4dbcb
Alejo Gschwind <alejogschwind.97@gmail.com>
2021-02-02 19:40:49 -0300

    ➕ Feat: Added Users actions.

--- client/src/redux/actions/usersActions.js
+++ client/src/redux/actions/usersActions.js
@@ -1,68 +1,107 @@
 import axios from "axios";
 
 import {
+  REQUEST_ACTION,
+  REQUEST_SUCCESS_ACTION,
+  REQUEST_FAILED_ACTION,
   GET_INSTRUCTORS, GET_STUDENTS,
   GET_USER, GET_USERS,
   UPDATE_USER
 } from "../constants/usersContants";
 
+const requestAction = () => ({
+  type: REQUEST_ACTION
+});
+
+const requestSuccessAction = () => ({
+  type: REQUEST_SUCCESS_ACTION
+});
+
+const requestFailedAction = (error) => ({
+  type: REQUEST_FAILED_ACTION,
+  error
+});
+
+const getUsersAction = (users) => ({
+  type: GET_USERS,
+  users
+});
+
+const getUserAction = (user) => ({
+  type: GET_USER,
+  user
+});
+
+const updateUserAction = (user) => ({
+  type: UPDATE_USER,
+  user
+});
+
 export const getUsers = () => {
-  return function (dispatch) {
-    axios.get("http://localhost:5000/users")
-      .then(res => {
-        dispatch({
-          type: GET_USERS,
-          payload: res
-        });
-      });
+  return function async(dispatch) {
+    try {
+      dispatch(requestAction());
+
+      const users = await axios.get("http://localhost:5000/users");
+
+      dispatch(getUsersAction(users));
+      dispatch(requestSuccessAction());
+    } catch (error) {
+      dispatch(requestFailedAction(error));
+    }
   };
 };
 
-export const getIntructors = () => {
-  return function (dispatch) {
-    axios.get("http://localhost:5000/users/instructors")
-      .then(res => {
-        dispatch({
-          type: GET_INSTRUCTORS,
-          payload: res
-        });
-      });
-  };
+// export const getIntructors = () => {
+//   return function (dispatch) {
+//     axios.get("http://localhost:5000/users/instructors")
+//       .then(res => {
+//         dispatch({
+//           type: GET_INSTRUCTORS,
+//           payload: res
+//         });
+//       });
+//   };
+// };
 
-};
+// export const getStudents = () => {
+//   return function (dispatch) {
+//     axios.get("http://localhost:5000/users/students")
+//       .then(res => {
+//         dispatch({
+//           type: GET_STUDENTS,
+//           payload: res
+//         });
+//       });
+//   };
+// };
 
-export const getStudents = () => {
-  return function (dispatch) {
-    axios.get("http://localhost:5000/users/students")
-      .then(res => {
-        dispatch({
-          type: GET_STUDENTS,
-          payload: res
-        });
-      });
-  };
-};
+export const getUserById = (id) => {
+  return function async(dispatch) {
+    try {
+      dispatch(requestAction());
+      const user = axios.get(`http://localhost:5000/users/user/${id}`);
 
-export const getOneUser = (id) => {
-  return function (dispatch) {
-    axios.get(`http://localhost:5000/users/user/${id}`)
-      .then(res => {
-        dispatch({
-          type: GET_USER,
-          payload: res.data
-        });
-      });
+      dispatch(getUserAction(user));
+      dispatch(requestSuccessAction());
+
+    } catch (error) {
+      dispatch(requestFailedAction(error));
+    }
   };
 };
 
 export const updateUser = (id, data) => {
   return function (dispatch) {
-    axios.patch(`http://localhost:5000/users/user/${id}`, data)
-      .then(res => {
-        dispatch({
-          type: UPDATE_USER,
-          payload: res.data
-        });
-      });
+    try {
+      dispatch(requestAction());
+
+      const updatedUser = axios.patch(`http://localhost:5000/users/user/${id}`, data);
+
+      dispatch(updateUserAction(updatedUser));
+      dispatch(requestSuccessAction());
+    } catch (error) {
+      dispatch(requestFailedAction(error));
+    }
   };
 };
