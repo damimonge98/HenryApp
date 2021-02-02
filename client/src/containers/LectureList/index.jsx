14ad8050c4e79cf8e1947880de29131c726efead
Sloot04 <suyaislootmans@gmail.com>
2021-02-01 23:03:43 -0300

    carga un video a una lecture

--- client/src/containers/LectureList/index.jsx
+++ client/src/containers/LectureList/index.jsx
@@ -13,6 +13,14 @@ const lectureList = () => {
     }]);
     const [estadoId, setEstadoId] = useState('');
 
+    const [video, setVideo] = useState({
+        title: '',
+        profesor: '',
+        url: '',
+        img: 'https://media-exp1.licdn.com/dms/image/C4E0BAQGy6GZmHb_SXA/company-logo_200_200/0/1603651276024?e=1619654400&v=beta&t=kRb_lMNqQF3oGVL9IrNYVxKdJf1qDW3FNTRdSeIu4zI',
+        duration: ''
+    });
+
     const [oneLecture, setOneLecture] = useState({
         title: "",
         description: ""
@@ -55,6 +63,28 @@ const lectureList = () => {
         });
     };
 
+    const handleVideoSubmit = () => {
+        const { title, profesor, url, img, duration } = video
+        axios.post(`http://localhost:5000/videos/${estadoId}`, { title, profesor, url, img, duration })
+            .then(res=>{
+                getLectures(),
+                setVideo({
+                    title: '',
+                    profesor: '',
+                    url: '',
+                    img: 'https://media-exp1.licdn.com/dms/image/C4E0BAQGy6GZmHb_SXA/company-logo_200_200/0/1603651276024?e=1619654400&v=beta&t=kRb_lMNqQF3oGVL9IrNYVxKdJf1qDW3FNTRdSeIu4zI',
+                    duration: ''
+                })
+
+            })
+    };
+
+    function handleVideoChange(e) {
+        setVideo({
+            ...video,
+            [e.target.name]: e.target.value
+        });
+    }
 
     return (
         <div>
@@ -65,17 +95,19 @@ const lectureList = () => {
                             <th scope="col">Modulo</th>
                             <th scope="col">Lecture</th>
                             <th scope="col">Descripcion</th>
+                            <th scope="col">Videos</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             allLectures.map((lecture, index) => {
-                                const { modulo, title, _id, description } = lecture;
+                                const { modulo, title, _id, description, video } = lecture;
                                 return (
                                     <tr key={index}>
                                         <td>{modulo}</td>
                                         <td>{title}</td>
                                         <td>{description}</td>
+                                        <td>{video.length}</td>
                                         <td ><button onClick={() => setEstadoId(_id)} ><a href="#openModal"> <i className="fas fa-user-edit" /></a></button></td>
                                         <div id="openModal" class="modalDialog">
                                             <div>	<a href="#close" title="Close" class="close">X</a>
@@ -83,7 +115,7 @@ const lectureList = () => {
                                                 <form onSubmit={() => handleSubmit(estadoId)}><a href="#close" title="Close" class="close"></a>
                                                     <p>
                                                         Titulo
-                                                        <input name="title" onChange={e => handleChange(e)} />
+                                                        <input name="title" type="text" onChange={e => handleChange(e)} />
                                                     </p>
                                                     <p>
                                                         Descripcion
@@ -92,22 +124,91 @@ const lectureList = () => {
                                                     <button type="submit"  >GUARDAR CAMBIOS</button>
                                                 </form>
                                             </div>
-
-
-
-
-
                                         </div>
+                                            {/* --------------------------------------------------------------------- */}
                                         <td><button type="submit" onClick={() => handleDelete(_id)} > <i className="fas fa-trash-alt" /></button></td>
-                                        <td><button type="submit"><i className="fas fa-plus-circle me-2" /> Agregar video</button></td>
+                                        <td><button onClick={() => setEstadoId(_id)}><a href="#openModal2"><i className="fas fa-plus-circle me-2" /> Agregar video</a></button></td>
+                                        <div id="openModal2" className="modalDialog">
+                                            <div>	<a href="#close" title="Close" className="close">X</a>
+                                                <div >
+                                                    <form onSubmit={()=>handleVideoSubmit(estadoId)}><a href="#close" title="Close" className="close"></a>
+                                                        <div >
+                                                            <label >
+                                                                Titulo</label>
+                                                            <div >
+                                                                <input
+                                                                    onChange={(e) => { handleVideoChange(e) }}
+                                                                    name="title"
+                                                                    type="text"
+                                                                    required
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                        <div >
+                                                            <label >
+                                                                Profesor</label>
+                                                            <div >
+                                                                <input
+                                                                    onChange={(e) => { handleVideoChange(e) }}
+                                                                    name="profesor"
+                                                                    type="text"
+                                                                    required
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                        <div >
+                                                            <label >
+                                                                url
+                                                        </label>
+                                                            <div >
+                                                                <input
+                                                                    onChange={(e) => { handleVideoChange(e) }}
+                                                                    name="url"
+                                                                    type="text"
+                                                                    required
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                        <div >
+                                                            <label >
+                                                                Duración
+                                                        </label>
+                                                            <div >
+                                                                <input
+                                                                    onChange={(e) => { handleVideoChange(e) }}
+                                                                    name="duration"
+                                                                    type="text"
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                        <div >
+                                                            <label >
+                                                                Imagen
+                                                        </label>
+                                                            <div >
+                                                                <input
+                                                                    onChange={(e) => { handleVideoChange(e) }}
+                                                                    name="img"
+                                                                    type="text"
+                                                                />
+                                                            </div>
+                                                        </div>
+                                                        <div>
+                                                            <div>
+                                                                <button type='submit'>
+                                                                    Cargar video
+                                                          </button>
+                                                            </div>
+                                                        </div>
+                                                    </form><br />
+                                                </div >
+                                            </div>
+                                        </div>
                                     </tr>)
                             })}
                     </tbody>
                 </table>
             </div>
-
-
-
         </div >
     )
 };
