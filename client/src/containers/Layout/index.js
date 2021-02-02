e53a14371504bcb4c9eaacd28676bbc6377ca6d6
Sloot04 <suyaislootmans@gmail.com>
2021-02-02 11:32:07 -0300

    Si no estas logeado no muestra los modulos

--- client/src/containers/Layout/index.js
+++ client/src/containers/Layout/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
 import { LayoutWrapper } from './styles';
 import Header from '../../components/Header';
 import Modules from '../Modulos/index'
 
 const Layout = ({ children }) => {
+  const { isAuth } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+
   return (
     <LayoutWrapper>
       <Header />
       {children}
-      <Modules ></Modules>
+      {
+        isAuth ?
+          <Modules ></Modules>
+          : console.log('')
+      }
     </LayoutWrapper>
   );
 };
