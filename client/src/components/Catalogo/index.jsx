63e2e4eaddb01be5b5ea9764b3f870113bb1b962
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-04 16:29:33 -0300

    redux

--- client/src/components/Catalogo/index.jsx
+++ client/src/components/Catalogo/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {useDispatch} from 'react-redux';
 // Components
 import OfferCard from '../OfferCard/index';
 import Layout from '../../containers/Layout';
+//import {getEmpleos} from '';
 // Styled Components
 import { CatalogueWrapper, EmpleosColumn } from './styles';
 
+//crear action que traiga los empleos
 
 const Catalogo = ()=>{
   const [empleo, setEmpleo] = useState([{
@@ -17,6 +20,17 @@ const Catalogo = ()=>{
     end: "",
     linkedIn:""
 }]);
+const dispatch = useDispatch();
+
+// useEffect(() => {
+//   getAllEmpleos();
+// }, []);
+
+// const getAllEmpleos = () => {
+//   setEmpleo([])
+//   dispatch(getEmpleos());
+// };
+
   
   return (
     <Layout>
