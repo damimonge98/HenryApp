aeffa87b9cfb7d9d50952ea92ae863e7b9c91c61
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-05 16:41:16 -0300

    puse el index de Chustis

--- client/src/containers/CRUDmodules/index.jsx
+++ client/src/containers/CRUDmodules/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import CreateModule from '../../components/CreateModule';
+import CreateModule from '../../components/CreateModule/index';
 import "./estilos.css";
 
 const ModuleList = () => {
