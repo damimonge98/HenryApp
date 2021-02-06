92bb623451a581b99e3a63082b9636c3a65e572b
damimonge98 <69829575+damimonge98@users.noreply.github.com>
2021-02-06 12:56:37 -0300

    video

--- client/src/components/OneLecture/index.js
+++ client/src/components/OneLecture/index.js
@@ -4,8 +4,12 @@ import axios from "axios";
 import "./index.css";
 import { Link } from "react-router-dom";
 import Readme from "../Readme/Readme";
+import { getAllLectures } from "../../redux/actions/lecturesActions";
+import {useSelector, useDispatch} from "react-redux";
 
 const OneLecture = (props) => {
+  const dispatch = useDispatch()
+  const allLectures = useSelector (state => state.lectures.lectures)
 
   const [allVideos, setAllVideos] = useState([{
     _id: "",
@@ -24,6 +28,7 @@ const OneLecture = (props) => {
   });
 
   useEffect(() => {
+    dispatch(getAllLectures())
     getVideos();
     getOneLecture();
   }, []);
@@ -43,17 +48,34 @@ const OneLecture = (props) => {
     );
   };
 
+  console.log("allVideos", allVideos)
+  var urlVideo = allVideos[0].url
+  urlVideo = urlVideo.replace("https://vimeo.com/", "")
+  console.log(urlVideo)
+  var urlVimeo = "https://player.vimeo.com/video/307791576?title=0&byline=0&portrait=0"
+  urlVimeo = urlVimeo.replace("307791576", urlVideo)
+
+
   return (
-    <div className = "containerDiv">
-      <h2 className = "h1">{lecture.title}</h2>
-      <br />
-      <button onClick = {()=> {window.history.back()}}className = "h1"> Volver </button>
-      <br/>
+    <div>      
       <Link to={{ pathname: lecture.urlLecture }} target="_blank">
-        <h4 className = "h1"><i className="fab fa-github"></i>{lecture.title}</h4>
+        <h1 className = "h1"><i className="fab fa-github"></i>{lecture.title}</h1>
       </Link>
       <br />
-      <div className="videoCard-grid">
+
+      <div className = "menuDiv">
+      {allLectures.length === 0 ? null :
+      allLectures.map(el => <button>{el.title}</button>)}
+     </div>
+
+      <button onClick = {()=> {window.history.back()}}className = "h1"> Volver </button>
+      <br/>
+      <div>
+        
+
+      </div>
+      
+      {/* <div className="videoCard-grid">
         {allVideos.map((video, index) => {
           return (
             <div key={index}>
@@ -61,6 +83,10 @@ const OneLecture = (props) => {
             </div>
           );
         })}
+      </div>*/}
+
+      <div className = "video" >
+        <iframe src= "https://player.vimeo.com/video/426051769" width="640" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
       </div>
       <Readme title = {lecture.title} url = {lecture.urlLecture}/>
     </div >
--- client/src/components/Readme/Readme.jsx
+++ client/src/components/Readme/Readme.jsx
@@ -1,12 +1,10 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 import { Remarkable } from 'remarkable';
-import "./readme.css"
+import "./readme.css";
+
 
 export default function Readme (props) {
-    console.log (props.title);
-    console.log (props.url)
-
     const [readme, setReadme] = useState(null)
     const md = new Remarkable();
   
@@ -14,7 +12,6 @@ export default function Readme (props) {
     ReadmeUrl = ReadmeUrl.replace("https://github.com/","https://api.github.com/repos/" )
     ReadmeUrl = ReadmeUrl.replace("/tree/master", "/contents")
     ReadmeUrl = ReadmeUrl + "/README.md?ref=master"
-    console.log(ReadmeUrl)
 
 
     const getRepoReadme = function () {
@@ -23,32 +20,27 @@ export default function Readme (props) {
           "Authorization": "token 2a0f242d2cf120c65ad72c8ba5804bf5404eff3c"
         }
       })
-      .then ((res) => 
-      setReadme(res.data.content))};
+      .then ((res) => setReadme(res.data.content))};
 
     useEffect (()=> {
         getRepoReadme()
     }, [ReadmeUrl])
     
-    console.log(readme)
-
     var codeReadme = atob(readme)
-    console.log(codeReadme)
     var newReadme = md.render(codeReadme)
     const firstTag = newReadme.indexOf("<h2>")
-    console.log(firstTag)
     const deleteHTML = newReadme.substring(firstTag)
     newReadme = deleteHTML
-    console.log(newReadme)
     
    
     return (
+      
          <div className = "readme">
            {!readme? <div class="spinner-border text-light" role="status"></div>
            :
            <Fragment>
             <div className = "readme">
-           <h1>Readme</h1>
+           <h1 className = "h1">Readme</h1>
            <div dangerouslySetInnerHTML={{__html: newReadme }}/>
            </div>
            </Fragment>
