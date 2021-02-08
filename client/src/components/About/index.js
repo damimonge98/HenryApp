9811b7d13fb6d9d8994b6abf27a491a687e69395
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-08 10:51:03 -0300

    formato seteado

---
+++ client/src/components/About/index.js
@@ -0,0 +1,173 @@
+import React from "react";
+import Layout from "../../containers/Layout";
+import './styles.css'
+
+export default function About() {
+  return (
+    <Layout>
+      <div className="henry">
+        <div className="nosotros">
+          <div className="divisor1"/>
+        <h1 className="title1">Sobre Nosotros</h1>
+          <div className="divisor1"/>
+        <p>
+            Henry es un startup que nació para dar oportunidades a personas con
+            determinación, disciplina y ambición de tener éxito en el mundo de
+            la tecnología. Brindamos acceso a la educación en carreras
+            tecnológicas a través de un modelo innovador de financiamiento.
+            Invertimos en el talento digital del futuro. En Henry, creemos que
+            la educación es el dinero mejor invertido. Por eso, trabajamos todos
+            los días para poder invertir en potencial humano, independientemente
+            de sus circunstancias. #SoyHenry
+          </p>
+        </div>
+        <div className="divisor1"/>
+        <div className="redes">
+          <h2 className="title2">¡Síguenos en nuestras redes!</h2>
+          <div className="divisor1"/>
+          <a href="https://twitter.com/soyhenry_ok" target="_blank">
+            Twitter
+          </a>
+          <a href="https://www.instagram.com/soyhenry_ok/" target="_blank">
+            Instagram
+          </a>
+          <a
+            href="https://www.linkedin.com/school/soyhenry/about/"
+            target="_blank"
+          >
+            Linkedin
+          </a>
+          <a
+            href="https://www.facebook.com/HENRY-108437840594440"
+            target="_blank"
+          >
+            Facebook
+          </a>
+          <h6>Contacto: contacto@soyhenry.com</h6>
+        </div>
+      </div>
+      <div className="divisor2"/>
+      <div className="pagina">
+        <h2 className="title2">Sobre esta página</h2>
+        <div className="divisor1"/>
+        <p>
+          Esta página es un proyecto creado por estudiantes de HENRY del cohorte
+          07 (Febrero 2021) como parte de un trabajo práctico desarrollado para
+          el Bootcamp. No es un sitio web real, funcional ni oficial. Todos sus
+          componentes fueron creados y/o seleccionados con fines demostrativos.
+        </p>
+        <div className="divisor2"/>
+        <h3 className="title3">Conoce a sus desarrolladores en sus redes sociales</h3>
+      </div>
+      <div className="divisor1"/>
+      <div className="devs">
+        <div className="dev">
+        Suyai Slootmans{" "}
+            <a
+              href="https://www.linkedin.com/in/suyai-slootmans/"
+              target="_blank"
+            >
+              Linkedin
+            </a>
+            <a
+              href="https://github.com/Sloot04"
+              target="_blank"
+            >
+              Github
+            </a>
+        </div>
+        <div className="dev">
+        Alejo Gschwind{" "}
+            <a
+              href="https://www.linkedin.com/in/gschwindalejo/"
+              target="_blank"
+            >
+              Linkedin
+            </a>
+            <a
+              href="https://github.com/AlejoGschwind"
+              target="_blank"
+            >
+              Github
+            </a>
+        </div>
+        <div className="dev">
+        Damian Monge{" "}
+            <a href="https://www.linkedin.com/in/damian-monge/" target="_blank">
+              Linkedin
+            </a>
+            <a
+              href="https://github.com/damimonge98"
+              target="_blank"
+            >
+              Github
+            </a>
+        </div>
+        <div className="dev">
+        Augusto Felicenzi{" "}
+            <a
+              href="https://www.linkedin.com/in/augusto-felicenzi/"
+              target="_blank"
+            >
+              Linkedin
+            </a>
+            
+            <a
+              href="https://github.com/Au-Fel"
+              target="_blank"
+            >
+              Github
+            </a>
+        </div>
+        <div className="dev">
+        Nereo Carbonara
+            <a
+              href="https://www.linkedin.com/in/nereo-carbonara/"
+              target="_blank"
+            >
+              Linkedin
+            </a>
+            <a
+              href="https://github.com/Le127"
+              target="_blank"
+            >
+              Github
+            </a>
+        </div>
+        <div className="dev">
+        Sergio Pepe
+            <a
+              href="https://www.linkedin.com/in/sergio-pepe-682baa15a/"
+              target="_blank"
+            >
+              Linkedin
+            </a>
+            <a
+              href="https://github.com/SergioPepe"
+              target="_blank"
+            >
+              Github
+            </a>
+        </div>
+        <div className="dev">
+        Ana Paula Barua{" "}
+            <a
+              href="https://www.linkedin.com/in/ana-paula-barua-188013131/"
+              target="_blank"
+            >
+              Linkedin
+            </a>
+            <a
+              href="https://github.com/AlejoGschwind"
+              target="_blank"
+            >
+              Github
+            </a>
+        </div>
+        <div className="divisor2"/>
+      </div>
+      <div className="divisor2"/>
+
+    </Layout>
+  );
+}
