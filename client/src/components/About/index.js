d3d16a546d72ee03352861cb2831eed48b4c73a0
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-08 13:59:42 -0300

    dev

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -64,7 +64,7 @@ export default function About() {
         <div className="dev">
         
         <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGMhX55gLDfaw/profile-displayphoto-shrink_200_200/0/1600636494618?e=1618444800&v=beta&t=Z4X637Id8on77ZDzAjxeHjTvNWwyHTktV0HyYeWlpGA" alt="su"/>
-        <h3 className="title3">Suyai Slootmans</h3>
+        <h3 className="title4">Suyai Slootmans</h3>
             <a
               href="https://www.linkedin.com/in/suyai-slootmans/"
               target="_blank"
@@ -80,7 +80,7 @@ export default function About() {
         </div>
         <div className="dev">
           <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHeRmsMYz18Mw/profile-displayphoto-shrink_200_200/0/1600875918077?e=1618444800&v=beta&t=_xYqxrbbbJA2Gr9-ydOZmfyuDcjTnuAY48hYB-NuNNQ" alt="ale"/>
-          <h3 className="title3">Alejo Gschwind</h3>
+          <h3 className="title4">Alejo Gschwind</h3>
             <a
               href="https://www.linkedin.com/in/gschwindalejo/"
               target="_blank"
@@ -96,7 +96,7 @@ export default function About() {
         </div>
         <div className="dev">
           <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHHEVp6NAcf_Q/profile-displayphoto-shrink_200_200/0/1611065707974?e=1618444800&v=beta&t=IXHrgeDFwkuGkCXHcPCTPVUAv3b6W4RdJhX87vIkBhg" alt="dami"/>
-          <h3 className="title3">Damian Monge</h3>
+          <h3 className="title4">Damian Monge</h3>
             <a href="https://www.linkedin.com/in/damian-monge/" target="_blank">
             <i class="fab fa-linkedin-in"/>
             </a>
@@ -109,7 +109,7 @@ export default function About() {
         </div>
         <div className="dev">
           <img src="https://media-exp1.licdn.com/dms/image/C5603AQEeBWZ99PnIQQ/profile-displayphoto-shrink_200_200/0/1611256090783?e=1618444800&v=beta&t=AGdEz4G8-RrOgY4BcvFUZQysVoU_ei9SFp1PlNorvHc" alt="chusto"/>
-          <h3 className="title3">Augusto Felicenzi</h3>
+          <h3 className="title4">Augusto Felicenzi</h3>
             <a
               href="https://www.linkedin.com/in/augusto-felicenzi/"
               target="_blank"
@@ -126,7 +126,7 @@ export default function About() {
         </div>
         <div className="dev">
           <img src="https://media-exp1.licdn.com/dms/image/C4E03AQFc-HueGEGfvA/profile-displayphoto-shrink_200_200/0/1600637380061?e=1618444800&v=beta&t=kWC483DAfYGXAqlnvn5u0M5Gr9WyOfeKPVAGY8va0tg" alt="nere"/>
-          <h3 className="title3">Nereo Carbonara</h3>
+          <h3 className="title4">Nereo Carbonara</h3>
             <a
               href="https://www.linkedin.com/in/nereo-carbonara/"
               target="_blank"
@@ -142,7 +142,7 @@ export default function About() {
         </div>
         <div className="dev">
           <img src="https://media-exp1.licdn.com/dms/image/C4E35AQFZ3V0XTN_1pw/profile-framedphoto-shrink_200_200/0/1610649173015?e=1612879200&v=beta&t=suC0j_s9mWJnLIVCrmbP-BV42WiOB0oOakQYpXr5j1Q" alt="ser" />
-          <h3 className="title3">Sergio Pepe</h3>
+          <h3 className="title4">Sergio Pepe</h3>
             <a
               href="https://www.linkedin.com/in/sergio-pepe-682baa15a/"
               target="_blank"
@@ -158,7 +158,7 @@ export default function About() {
         </div>
         <div className="dev">
           <img src="https://media-exp1.licdn.com/dms/image/C4E03AQHPxkhz29TPeg/profile-displayphoto-shrink_200_200/0/1611190654795?e=1618444800&v=beta&t=sFR3PAmdpPe-lHsKgsu1rQSlKGRFD4gX40g0CxV-smE" alt="ana"/>
-          <h3 className="title3">Ana Paula Barua</h3>
+          <h3 className="title4">Ana Paula Barua</h3>
             <a
               href="https://www.linkedin.com/in/ana-paula-barua-188013131/"
               target="_blank"
