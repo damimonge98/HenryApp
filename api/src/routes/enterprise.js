18c6a832ca8ff3397fbc15500c4cae2ffc9130ba
Sloot04 <suyaislootmans@gmail.com>
2021-02-08 14:48:51 -0300

    rutas de empresas

--- api/src/routes/enterprise.js
+++ api/src/routes/enterprise.js
@@ -26,6 +26,48 @@ router.get('/:id', (req, res) => {
         );
 });
 
+//Create one enterprise
+router.post('/', async (req, res) => {
+    const { name, logo, password, email } = req.body;
+    const empresa = new Empresa({
+      name, 
+      logo,
+      password,
+      email
+    });
+    
+    try {
+      const newEmpresa = await empresa.save();
+      res.json(newEmpresa);
+    } catch (error) {
+      res.status(400).json({ message: error.message });
+    }
+  });
 
+  //Update one enterprise
+  router.patch('/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, logo, password, email } = req.body;
+     
+    Empresa.findByIdAndUpdate(id, req.body, { new: true }).then(empresa => {
+      res.json(empresa);
+    })
+      .catch(error => {
+        res.status(400).json({ message: error.message });
+      });
+  });
+
+  //Delete one enterprise
+  router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    
+    Empresa.findById(id).then(empresa => {
+     empresa.remove();
+  
+      res.json({ message: 'Enterprise has been deleted' });
+    }).catch(error => {
+      res.status(500).json({ message: error.message });
+    });
+  });
 
 module.exports = router;
