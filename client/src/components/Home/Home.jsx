9948e910f831efaca3b3e0e1d740cc35cd0b8167
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-09 20:21:52 -0300

    home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -10,52 +10,24 @@ const InitialHome = () => {
       <div className="div1">
         <Booms />
       </div>
-
+      <div className="container">
       <div className="div2">
-        <img src={HenryDev} className="devImg" />
+        <h1>Invertimos en tu educación</h1>
+        <p className="parrafo">
+          Conviértete en un desarrollador de software en 4 meses a remoto. Y lo
+          mejor, sólo nos pagas cuando consigues un trabajo.
+        </p>
+        <button className="btnhome">Empeza tu carrera</button>
       </div>
-
       <div className="div3">
-        <div className="divContainer3">
-          <h1> ¿Por qué Henry?</h1>
-          <br />
-          <h2>100% online</h2>
-          <br />
-          <p>
-            Toda la carrera es a remoto pero las clases son en VIVO. Solo
-            necesitas una computadora y un buen acceso a internet.
-          </p>
-          <br />
-          <h2>Soporte 1:1</h2>
-          <br />
-          <p>
-            Contamos con una red de instructores, PM’s y coaches que van a estar
-            a tu disposición cuando tengas dificultades ayudándote a acelerar tu
-            carrera.
-          </p>
-          <br />
-          <h2>Currícula</h2>
-
-          <br />
-          <p>
-            Con +700 horas de programación basado en prácticas y proyectos
-            reales, aprenderás las habilidades más demandadas del mercado
-            laboral.
-          </p>
-          <br />
-          <h2>Comunidad</h2>
-          <br />
-          <p>
-            Unirte a Henry significa crear conexiones para toda la vida, hacer
-            nuevos amigos y aprovechar oportunidades globales y diversas.
-          </p>
-        </div>
+        <h1>Hire Henrys</h1>
+        <p className="parrafo">
+          Si tienes una empresa y buscas sumar programadores a tu equipo,
+          ofrecemos un espacio donde puedes publicar tus ofertas y recibir
+          postulaciones de nuestros graduados.
+        </p>
+        <button className="btnhome">Publica tus ofertas</button>
       </div>
-
-      <div className="div4">
-        <div>
-          <img src={Cost} className="cost" />
-        </div>
       </div>
     </div>
   );
