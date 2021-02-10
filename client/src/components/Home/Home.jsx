788e932f73572e37413257949b6be2870323dc2d
artoria-95 <64420864+artoria-95@users.noreply.github.com>
2021-02-09 21:05:22 -0300

    responsive home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import HenryDev from "../../assets/images/HenryDev.png";
-import Cost from "../../assets/images/Cost.png";
+import {Link} from "react-router-dom";
 import Booms from "../Booms/Booms";
 import "./styles.css";
 
@@ -11,23 +10,29 @@ const InitialHome = () => {
         <Booms />
       </div>
       <div className="container">
-      <div className="div2">
-        <h1>Invertimos en tu educación</h1>
-        <p className="parrafo">
-          Conviértete en un desarrollador de software en 4 meses a remoto. Y lo
-          mejor, sólo nos pagas cuando consigues un trabajo.
-        </p>
-        <button className="btnhome">Empeza tu carrera</button>
-      </div>
-      <div className="div3">
-        <h1>Hire Henrys</h1>
-        <p className="parrafo">
-          Si tienes una empresa y buscas sumar programadores a tu equipo,
-          ofrecemos un espacio donde puedes publicar tus ofertas y recibir
-          postulaciones de nuestros graduados.
-        </p>
-        <button className="btnhome">Publica tus ofertas</button>
-      </div>
+        <div className="div2">
+          <h1>Recursos para los alumnos</h1>
+          <p className="parrafo">
+            Tendrás acceso a todo tu material de estudio según el módulo en el
+            que estás cursando, clases grabadas de diferentes instructores y tu
+            estado de cuenta, y cuando termines el Módulo 4 ¡Podrás acceder a
+            nuestra bolsa de empleos!
+          </p>
+          <Link to="/login">
+          <button className="btnhome">Accedé</button>
+          </Link>
+        </div>
+        <div className="div3">
+          <h1>Hire Henrys</h1>
+          <p className="parrafo">
+            Si tienes una empresa y buscas sumar programadores a tu equipo,
+            ofrecemos un espacio donde puedes publicar tus ofertas y recibir
+            postulaciones de nuestros graduados.
+          </p>
+          <Link to="/login">
+          <button className="btnhome">Publica tus ofertas</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
