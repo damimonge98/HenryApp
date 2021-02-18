fc1954f229bcc4beb63a363d3ee3ac7c525b310d
Le127 <nereocarbonara@gmail.com>
2021-02-18 13:02:19 -0300

    Modulo 1, falta img

--- api/data/index.js
+++ api/data/index.js
@@ -1,29 +1,43 @@
+
+
 const modules = [
   {
     title: "Preparation",
-    description: "Lorem ipsum dolor sit amet, consectetur.",
+    description: "Curso de preparacion para el ingreso a Henry",
     order: 0,
     lectures: [
       {
-        title: "React & Redux 1",
+        title: "01-Git",
+        description: "¿Qué es un control de versiones?",
+        imagen: "https://i.ibb.co/bL3tRjH/git-logo.png",
+        urlLecture: "https://github.com/Le127/Curso.Prep.Henry/tree/master/01-Git",
+      },
+      {
+        title: "02-JS-I",
+        description: "Introducción a Javascript",
+        imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
+        urlLecture: "https://github.com/Le127/Curso.Prep.Henry/tree/master/02-JS-I",
+      },
+      {
+        title: "03-JS-II",
+        description: "Javascript II",
+        imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
+        urlLecture: "https://github.com/Le127/Curso.Prep.Henry/tree/master/03-JS-II",
+      },
+      {
+        title: "04-JS-III",
+        description: "Javascript III",
+        imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
+        urlLecture: "https://github.com/Le127/Curso.Prep.Henry/tree/master/04-JS-III",
+      },
+      {
+        title: "React & Redux 5",
         description: "Lorem ipsum dolor sit amet, consectetur.",
         imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
         urlLecture: "https://github.com/soyHenry/FT-M2/tree/master/02-CSS",
       },
       {
-        title: "React & Redux 2",
-        description: "Lorem ipsum dolor sit amet, consectetur.",
-        imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
-        urlLecture: "https://github.com/soyHenry/FT-M2/tree/master/02-CSS",
-      },
-      {
-        title: "React & Redux 3",
-        description: "Lorem ipsum dolor sit amet, consectetur.",
-        imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
-        urlLecture: "https://github.com/soyHenry/FT-M2/tree/master/02-CSS",
-      },
-      {
-        title: "React & Redux 4",
+        title: "React & Redux 5",
         description: "Lorem ipsum dolor sit amet, consectetur.",
         imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
         urlLecture: "https://github.com/soyHenry/FT-M2/tree/master/02-CSS",
@@ -38,39 +52,57 @@ const modules = [
   },
   {
     title: "Module I",
-    description: "Lorem ipsum dolor sit amet, consectetur.",
+    description: "Foundations",
     order: 1,
     lectures: [
       {
-        title: "React & Redux 6",
-        description: "Lorem ipsum dolor sit amet, consectetur.",
+        title: "00-IntroToCS",
+        description: "Ciencias de la Computacion",
         imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
-        urlLecture: "https://github.com/soyHenry/FT-M2/tree/master/02-CSS",
+        urlLecture: "https://github.com/soyHenry/FT-M1/tree/master/00-IntroToCS",
       },
       {
-        title: "React & Redux 7",
-        description: "Lorem ipsum dolor sit amet, consectetur.",
+        title: "01-JavaScriptAvanzado-I",
+        description: "Entendiendo JavaScript a Fondo I",
         imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
-        urlLecture: "https://github.com/soyHenry/FT-M2/tree/master/02-CSS",
+        urlLecture: "https://github.com/soyHenry/FT-M1/tree/master/01-JavaScriptAvanzado-I",
       },
       {
-        title: "React & Redux 8",
-        description: "Lorem ipsum dolor sit amet, consectetur.",
+        title: "02-JavaScriptAvanzado-II",
+        description: "Entendiendo JavaScript a Fondo II",
         imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
-        urlLecture: "https://github.com/soyHenry/FT-M2/tree/master/02-CSS",
+        urlLecture: "https://github.com/soyHenry/FT-M1/tree/master/02-JavaScriptAvanzado-II",
       },
       {
-        title: "React & Redux 9",
-        description: "Lorem ipsum dolor sit amet, consectetur.",
+        title: "03-EstructuraDeDatos-I",
+        description: "Estructuras de Datos I",
         imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
-        urlLecture: "https://github.com/soyHenry/FT-M2/tree/master/02-CSS",
+        urlLecture: "https://github.com/soyHenry/FT-M1/tree/master/03-EstructuraDeDatos-I",
       },
       {
-        title: "React & Redux 10",
-        description: "Lorem ipsum dolor sit amet, consectetur.",
+        title: "04-EstructuraDeDatos-II",
+        description: "Estructuras de Datos II",
         imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
-        urlLecture: "https://github.com/soyHenry/FT-M2/tree/master/02-CSS",
+        urlLecture: "https://github.com/soyHenry/FT-M1/tree/master/04-EstructuraDeDatos-II",
       },
+      {
+        title: "05-EstructuraDeDatos-III",
+        description: "Estructuras de Datos III",
+        imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
+        urlLecture: "https://github.com/soyHenry/FT-M1/tree/master/05-EstructuraDeDatos-III",
+      },
+      {
+        title: "06-Algoritmos-I",
+        description: "Algoritmos I",
+        imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
+        urlLecture: "https://github.com/soyHenry/FT-M1/tree/master/06-Algoritmos-I",
+      },
+      {
+        title: "06-Algoritmos-II",
+        description: "Algoritmos II",
+        imagen: "https://rosolutions.com.mx/blog/wp-content/uploads/2018/07/react-redux.png",
+        urlLecture: "https://github.com/soyHenry/FT-M1/tree/master/07-Algoritmos-II",
+      }
     ],
   },
   {
